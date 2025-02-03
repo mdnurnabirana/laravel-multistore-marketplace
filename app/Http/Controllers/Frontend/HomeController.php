9e8a995d657723b0eaa5e594628ff5c41d9faa88
Md. Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-02-04 00:23:38 +0600

    Show Type Based Product on Frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -9,6 +9,7 @@ use App\Models\Category;
 use App\Models\FlashSale;
 use App\Models\FlashSaleItem;
 use App\Models\HomePageSetting;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -20,14 +21,31 @@ class HomeController extends Controller
         $flashSaleItems = FlashSaleItem::where('show_at_home', 1)->where('status', 1)->get();
         $popularCategory = HomePageSetting::where('key', 'popular_category_section')->first();
         $brands = Brand::where('status', 1)->where('is_featured', 1)->get();
+        $typeBaseProducts = $this->getTypeBaseProduct();
         return view('frontend.home.home',
             compact(
                 'sliders',
                 'flashSaleDate',
                 'flashSaleItems',
                 'popularCategory',
-                'brands'
+                'brands',
+                'typeBaseProducts'
             )
         );
+    }
+
+    public function getTypeBaseProduct()
+    {
+        $typeBaseProducts = [];
+
+        $typeBaseProducts['new_arrival'] = Product::where(['product_type' => 'new_arrival', 'is_approved' => 1, 'status' => 1])->orderBy('id', 'DESC')->take(8)->get();
+        
+        $typeBaseProducts['feature_product'] = Product::where(['product_type' => 'featured_product', 'is_approved' => 1, 'status' => 1])->orderBy('id', 'DESC')->take(8)->get();
+    
+        $typeBaseProducts['top_product'] = Product::where(['product_type' => 'top_product', 'is_approved' => 1, 'status' => 1])->orderBy('id', 'DESC')->take(8)->get();
+    
+        $typeBaseProducts['best_product'] = Product::where(['product_type' => 'best_product', 'is_approved' => 1, 'status' => 1])->orderBy('id', 'DESC')->take(8)->get();
+        
+        return $typeBaseProducts;
     }
 }
