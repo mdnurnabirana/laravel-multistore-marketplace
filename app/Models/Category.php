9e80aca4a74087b6aaef892fe7c4efce19b4c71b
Md. Nurnabi Rana <100693911+mdnurnabirana@users.noreply.github.com>
2025-06-26 02:05:07 +0600

    Fix Typo Error in Category Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Subcategory;
+use App\Models\SubCategory;
 
 class Category extends Model
 {
